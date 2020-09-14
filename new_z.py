029a38614bb0a31785eb89d0d2c7fac04ea64ea0
bwelliver <bcwelliver@lbl.gov>
2020-09-13 17:21:26 -0700

    New tests

--- new_z.py
+++ new_z.py
@@ -40,10 +40,11 @@ def convert_to_numpy(data):
     return data
 
 
-@jit(nopython=True, parallel=True)
+@jit(nopython=True)
 def lock_in_amplifier(signal, freq, fs):
     """Compute lock-in amplifier components."""
     result = np.zeros(2)
+    # This is really 2*pi*f * t where t spans the duration of time the signal is
     t = (2*np.pi*np.linspace(0, signal.shape[1], signal.shape[1])/fs) * freq  # check this
     #xv = np.mean(signal*np.sin(t))
     #yv = np.mean(signal*np.cos(t))
@@ -83,10 +84,20 @@ def lock_in(v_in, v_out, dt, number_samples, exfreq):
             modphase = lock_in_amplifier(v_out, freq, fs)
             lock_vout_mod[idx] = modphase[0]
             lock_vout_phase[idx] = modphase[1]
-    return lock_vin_mod, lock_vin_phase, lock_vout_mod, lock_vout_phase
+    return lock_vin_mod, lock_vin_phase, lock_vout_mod, lock_vout_phase, freqs
 
 
-def main(filename):
+def compute_zmeas(fvin, fvout, squid_params, input_gain=1, output_gain=1):
+    """Compute the measured Z for a given set of observations."""
+    Rsh = squid_params['Rsh']
+    M = squid_params['M']
+    Rfb = squid_params['Rfb']
+    Zbias = squid_params['Zbias']
+    z_meas = (M*Rsh*Rfb*(fvin/input_gain))/(Zbias*(fvout/output_gain))
+    return z_meas
+
+
+def get_lockin_averages(filename):
     data = load_data(filename)
     data = convert_to_numpy(data)
     dt = data['SamplingWidth_s'][0]
@@ -94,3 +105,26 @@ def main(filename):
     exfreq = 17
     locked = lock_in(data['Waveform000'], data['Waveform001'], dt, number_samples, exfreq)
     return locked
+
+
+def transform_to_rect(polar_results):
+    """Transform to rectangular parameters."""
+    fvin = polar_results[0]*(np.cos(polar_results[1]) + 1j*np.sin(polar_results[1]))
+    fvout = polar_results[2]*(np.cos(polar_results[3]) + 1j*np.sin(polar_results[3]))
+    return fvin, fvout
+
+
+def main(filenames):
+    normal_file = '/Users/bwelliver/tmp/SlowDigit_Norm_250uA_1000nA_Square/root//SlowDigitNorm250uA1000nASquare_1.root'
+    sc_file = '/Users/bwelliver/tmp/SlowDigit_SC_0uA_1uA_Suqare/root//SlowDigitSC0uA1uASquare_1.root'
+    norm_locked = get_lockin_averages(normal_file)
+    sc_locked = get_lockin_averages(sc_file)
+    norm_fvin, norm_fvout = transform_to_rect(norm_locked)
+    sc_fvin, sc_fvout = transform_to_rect(sc_locked)
+    exfreq = 17
+    freqs = norm_locked[4]
+    cut = np.logical_and(np.remainder(freqs, exfreq) == 0, np.remainder(np.floor(freqs/exfreq), 2) == 1)
+    squid_params = {'Rsh': 20.2e-3, 'M': -1.3145, 'Rfb': 100000, 'Zbias': 10200}
+    norm_zmeas = compute_zmeas(norm_fvin, norm_fvout, squid_params, input_gain=1)
+    sc_zmeas = compute_zmeas(sc_fvin, sc_fvout, squid_params, input_gain=1)
+    return norm_fvin, norm_fvout, sc_fvin, sc_fvout, freqs, cut, norm_zmeas, sc_zmeas
