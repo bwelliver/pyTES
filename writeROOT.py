5c37d4b8b4079eeadb4d6505938b5e3bd09bb491
bwelliver <bwelliver@berkeley.edu>
2024-10-06 14:33:42 -0700

    rewrite of writeROOT to utilize typing and adjust vector strategy a bit

--- writeROOT.py
+++ writeROOT.py
@@ -1,60 +1,89 @@
-'''Module to write root files based on an input dictionary that mimics the data structure to be written'''
-import time
+"""Module to write root files based on an input dictionary that mimics the data structure to be written"""
+from typing import Any
+from collections.abc import Sequence
 import numpy as np
+import numpy.typing as npt
 
 from array import array
 import ROOT as rt
-# rt.gROOT.ProcessLine("#include<vector>")
-# rt.gROOT.ProcessLine("std::vector<double> vec;")
-# from ROOT import vec
 
 
-def writeTDir(source, dir_data, allowExistingDir=False):
+def writeTDir(source : Any, dir_data : dict[str, Any], allowExistingDir : bool=False) -> bool:
+    """Write contents of current TDirectory into file.
+
+    Arguments:
+        source -- Input filename or path to file to write into
+        dir_data -- dictionary describing TDirectory structure
+
+    Keyword Arguments:
+        allowExistingDir -- Whether existing directories are permissible (default: {False})
+
+    Returns:
+        _description_
+    """
     '''Write contents of current TDirectory into file
     dir_data is a dictionary whose keys are the directory names
     The values of each key represent data structures to put INTO the directory
     '''
-    for key, value in dir_data.items():
-        tdir = source.mkdir(key, key, allowExistingDir)
+    result = False
+    for dir_name, dir_values in dir_data.items():
+        tdir = source.mkdir(dir_name, dir_name, allowExistingDir)
         tdir.cd()
-        for subkey, subvalue in value.items():
-            if subkey == 'TDirectoryFile':
+        for dir_key, subvalue in dir_values.items():
+            if dir_key == 'TDirectoryFile':
                 result = writeTDirF(subvalue)
-            elif subkey == 'TDirectory':
+            elif dir_key == 'TDirectory':
                 result = writeTDir(tdir, subvalue)
-            elif subkey == 'TTree':
+            elif dir_key == 'TTree':
                 result = writeTTree(subvalue)
-            elif subkey == 'vector<double>':
+            elif dir_key == 'vector<double>':
                 result = writeVector(tdir, subvalue)
             else:
-                result = None
+                result = False
         del tdir
         source.cd()
         # print(rt.gDirectory.pwd())
     return result
 
 
-def writeVector(tdir, vecdata):
-    """Write a std::vector<double> object into the current directory directly."""
+def writeVector(tdir: Any, vecdata: dict[str, Any]) -> bool:
+    """Write a std::vector<double> object into the current directory directly.
+
+    Arguments:
+        tdir -- TDirectory object
+        vecdata -- dictionary describing the vectors to be written
+
+    Returns:
+        bool indicating success
+    """
+    std_vector = getattr(getattr(rt, 'std'), 'vector')
     for key, value in vecdata.items():
-        vec = rt.std.vector('double')(value.size)
+        vec = std_vector("double")()
         vec.assign(value)
         tdir.WriteObject(vec, key)
         vec.clear()
     return True
 
 
-def writeTVectorT(tdata):
-    '''Write a TObject into the ROOT file'''
+def writeTVectorT(tdata : dict[str, Any]) -> bool:
+    """Write a TObject into the currently open ROOT file
+
+    Arguments:
+        tdata -- dictionary describing the contents of the TObject and its data
+
+    Returns:
+        boolean describing success or failure
+    """
+    result = True
     for key, value in tdata.items():
-        TVectorT = getattr(rt, 'TVectorT')
-        tvector = TVectorT("double")(value.size, value.astype('double'))
+        TVectorT = getattr(rt, "TVectorT")
+        tvector = TVectorT("double")(value.size, value.astype("double"))
         tvector.Write(key)
         del tvector
-    return True
+    return result
 
 
-def writeTDirF(data):
+def writeTDirF(data : dict[str, Any]):
     '''Write contents of TDirectoryFile dictionary to a particular directory
     Directory file dictionary has list of TNamed objects that can be written
     Structure is a dictionary that contains 2 keys: TDirFileName and TNamed
@@ -70,81 +99,91 @@ def writeTDirF(data):
     return True
 
 
-def writeTTree(tdata):
+def writeTTree(tdata : dict[str, Any]) -> bool:
     '''Create and write a tree to the root file in whatever directory we are in
     Incoming tdata is a dictionary whose keys specify the tree names
     The value of a tree is a dictionary whose keys are branch names and values are branch values
     '''
+    result = False
     TTree = getattr(rt, 'TTree')
-    for key, value in tdata.items():
-        tree = TTree(key, key)
-        for subkey, subvalue in value.items():
+    for tree_name, tree_values in tdata.items():
+        tree = TTree(tree_name, tree_name)
+        for subkey, subvalue in tree_values.items():
             if subkey == 'TBranch':
-                writeTBranch(tree, subvalue)
+                result = result and writeTBranch(tree, subvalue)
         del tree
-    return True
+    return result
 
-
-def writeTBranch(tree, branch_data):
-    '''Create and write branches to the root file in whatever tree we have
+# branch_data['branchA'] = some kind of array
+# branch_data['branchB'] = dictionary of arrays
+# branch_data['branchC'] = 2d numpy array
+def writeTBranch(tree : Any, branch_data : dict[str, Sequence[float | int] | dict[int, Any] | npt.NDArray[np.float64]]) -> bool:
+    """Create and write branches to the root file in whatever tree.
     Incoming branch_data is a dictionary where the key specifies the branch name and the value is the branch value
-    If the incoming branch_data[key] is itself an array (either a dict of arrays or numpy.ndarray, 2D) it will be a vector<double> branch.
-    '''
-    AddressOf = getattr(rt, 'AddressOf')
-    std = getattr(rt, 'std')
-    dloc = {}
-    for branchkey in branch_data.keys():
-        if isinstance(branch_data[branchkey], dict) or (isinstance(branch_data[branchkey], np.ndarray) and len(branch_data[branchkey].shape) == 2):
-            if isinstance(branch_data[branchkey], dict):
-                nentries = len(branch_data[branchkey])
-            else:
-                nentries = branch_data[branchkey].shape[0]
-            print('branchkey: {}'.format(branchkey))
-            nsamples = branch_data[branchkey][0].size
-            # We can try to pre-allocate the vector by putting nentries at the end
-            dloc[branchkey] = std.vector('double')(nsamples)
-            # Caution: We may need to do something else here about the vectors
-            # print('The address of the vector is {}'.format(dloc[branchkey]))
-            tree.Branch(branchkey, 'std::vector<double>', AddressOf(dloc[branchkey]))
+    If the incoming branch_data[key] is itself an array (either a dict of arrays or numpy.ndarray, 2D) it will be 
+    a vector<double> branch. In this case the length of the dictionary should be the number of entries
+
+    Arguments:
+        tree -- ROOT TTree object
+        branch_data -- dictionary describing ROOT data
+
+    Returns:
+        Bool to indicate whether it succeeded or not
+    """
+    
+    # Again because pyROOT objects are made dynamically.
+    std_vector = getattr(getattr(rt, 'std'), 'vector')
+    dloc: dict[str, Any] = {}
+    scalar_branches: list[str] = []
+    vector_branches: list[str] = []
+    nentries = None
+    for branch_key, branch_value in branch_data.items():
+        is_vector = isinstance(branch_value, dict) or (
+            isinstance(branch_value, np.ndarray) and len(branch_value.shape) == 2)
+        nentries_branch = len(branch_value) if not isinstance(branch_value, dict) else len(branch_value)
+        if nentries is None:
+            nentries = nentries_branch
+        elif nentries_branch != nentries:
+            raise ValueError(f"Inconsistent number of entries for branch '{branch_key}'.")        
+        if is_vector:
+            dloc[branch_key] = std_vector("double")()
+            tree.Branch(branch_key, "std::vector<double>", dloc[branch_key])
+            vector_branches.append(branch_key)
         else:
-            nentries = branch_data[branchkey].size
-            if branchkey in ['Timestamp_s', 'Timestamp_mus', 'NumberOfSamples']:
-                dloc[branchkey] = array('I', [0])
-                tree.Branch(branchkey, dloc[branchkey], branchkey + '/i')
+            if branch_key in ["Timestamp_s", "Timestamp_mus", "NumberOfSamples"]:
+                dloc[branch_key] = array("I", [0])
+                tree.Branch(branch_key, dloc[branch_key], f"{branch_key}/i")
             else:
-                dloc[branchkey] = array('d', [0.0])
-                tree.Branch(branchkey, dloc[branchkey], branchkey + '/D')
-    # Now loop over the branches and fill them
-    # In the event the thing we are trying to fill is a vector nentries is supposed to be the number of things in vector, aka vector::size
-    # In the event the thing we are trying to fill is not a vector it is the number of entries in the array...should this be so?
-    # Ultimately nentries should be the number of distinct 'events' that there are
-    #print('There are {} entries to write.'.format(nentries))
-    for event in range(nentries):
-        for branchkey in branch_data.keys():
-            # We can have 2 types of entries -- scalar or vector
-            # dictionaries or np.ndarrays then it is a vector, otherwise scalar
-            if isinstance(branch_data[branchkey], dict):
-                #dloc[branchkey].assign(list(branch_data[branchkey][event].values()))
-                dloc[branchkey].assign(list(branch_data[branchkey][event]))
-            elif isinstance(branch_data[branchkey], np.ndarray) and len(branch_data[branchkey].shape) == 2:
-                #dtemp = branch_data[branchkey][event]
-                #print('For branch {} the type is {}'.format(branchkey, type(dtemp)))
-                #print("The shape of the temp data is {}".format(dtemp.shape))
-                dloc[branchkey].assign(branch_data[branchkey][event])
-            else:
-                if branchkey in ['Timestamp_s', 'Timestamp_mus', 'NumberOfSamples']:
-                    dloc[branchkey][0] = int(branch_data[branchkey][event])
+                dloc[branch_key] = array("d", [0.0])
+                tree.Branch(branch_key, dloc[branch_key], f"{branch_key}/D")
+            scalar_branches.append(branch_key)
+        # Branches named and set up -- now fill
+        for event in range(nentries):
+            # Parse the scalars first
+            for branch_name in scalar_branches:
+                if branch_name in ["Timestamp_s", "Timestamp_mus", "NumberOfSamples"]:
+                    dloc[branch_name][0] = int(branch_data[branch_name][event])
                 else:
-                    dloc[branchkey][0] = branch_data[branchkey][event]
+                    dloc[branch_name][0] = branch_data[branch_name][event]
+            # Parse the vectors
+            for branch_name in vector_branches:
+                dloc[branch_name].assign(branch_data[branch_name][event])
         tree.Fill()
-    tree.Write()
     return True
 
-
-def writeROOT(input_file, data, mode="RECREATE"):
-    '''Write a root file. This function works by passing in data as a dictionary. Dictionary keys should be "TDirectory", "TTree", "TBranch", and "TObject". Nested inside each can be appropriate items. For example a TTree key may contain as a value a dictionary of branches, or a TDirectory can contain a list of trees and other such things.
+def writeROOT(input_file : str, data : dict[str, Any], mode : str ="RECREATE") -> bool:
+    """Write a ROOT file. Write a root file. This function works by passing in data as a dictionary. Dictionary keys should be "TDirectory", "TTree", "TBranch", and "TObject". 
+    Nested inside each can be appropriate items. For example a TTree key may contain as a value a dictionary of branches, or a TDirectory can contain a list 
+    of trees and other such things.
     Example:
-    dict = {'TDirectory': {
+    dict = {'TDirectory': {'topLevel': {},'newDir': {'TTree':{'newTree': {'TBranch': {'branchA': 'branchA'}}}}},'TTree':{'tree1': {'TBranch': {'branch1': 'branch1', 'branch2': 'branch2'}}}}
+
+    In this way we basically encode the ROOT hiearchy into a dictionary 
+
+    Arguments:
+        input_file -- Name or path of root file to write.
+        data -- Data dictionary structured somewhat similar to:
+        dict = {'TDirectory': {
                 'topLevel': {},
                 'newDir': {
                     'TTree': {
@@ -164,9 +203,13 @@ def writeROOT(input_file, data, mode="RECREATE"):
                     }
                 }
             }
-    dict = {'TDirectory': {'topLevel': {},'newDir': {'TTree':{'newTree': {'TBranch': {'branchA': 'branchA'}}}}},'TTree':{'tree1': {'TBranch': {'branch1': 'branch1', 'branch2': 'branch2'}}}}
 
-    In this way we basically encode the ROOT hiearchy into a dictionary'''
+    Keyword Arguments:
+        mode -- What ROOT mode to pass in for file writing (default: {"RECREATE"})
+
+    Returns:
+        Nothing
+    """
 
     # Make the ROOT file first
     TFile = getattr(rt, 'TFile')
@@ -174,6 +217,7 @@ def writeROOT(input_file, data, mode="RECREATE"):
     allowExistingDir = mode == 'update'
     # tfile.SetCompressionLevel(0)
     # Now we parse the dictionary. It is a good idea here to separate things by type
+    result = False
     for key, value in data.items():
         if key == 'TDirectoryFile':
             result = writeTDirF(value)
@@ -184,7 +228,7 @@ def writeROOT(input_file, data, mode="RECREATE"):
         elif key == 'TVectorT':
             result = writeTVectorT(value)
         else:
-            result = None
+            result = False
         tfile.cd()
     del tfile
     return result
